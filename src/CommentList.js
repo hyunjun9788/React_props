8c08d21d75f743c96612a6cc51f04e3afd699903
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-23 17:22:57 +0900

    feat: 댓글 추가

--- src/CommentList.js
+++ src/CommentList.js
@@ -5,6 +5,7 @@ function CommentList(props) {
     return (
         <div>
             <Comment name={'남현준'} comment={'안녕하세요, 남현준입니다.'}/>
+            <Comment name={'김진수'} comment={'리액트 재미있어요'}/>
         </div>
     )
 }
