04ed95b9991a681ce0e4b041557f1ed4e7316187
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-23 17:27:32 +0900

    feat: map 함수 활용

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Comment from "./Comment";
 
+
+const comments = [
+    {
+        name: '남현준',
+        comment: '안녕하세요, 남현준입니다.'
+    },
+    {
+        name: '김진수',
+        comment: '리액트 재미있어요.'
+    },
+    {
+        name: '김민재',
+        comment: '리액트 어려워요.'
+    }
+]
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name={'남현준'} comment={'안녕하세요, 남현준입니다.'}/>
-            <Comment name={'김진수'} comment={'리액트 재미있어요'}/>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}/>
+                )
+            })}
         </div>
     )
 }
